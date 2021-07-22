408fdcc1e4893f4164f624dbb26d8c67b16b090b
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2021-07-22 09:33:24 -0400

    move renamed files to output folder

--- RenameFromCSV.ps1
+++ RenameFromCSV.ps1
@@ -8,10 +8,25 @@ $csv = Read-Host -Prompt "Drag and drop CSV file and remove any quotes around fi
 $csvnames = Import-Csv -Path $csv
 $videos = Get-ChildItem -Path $path
 
+# create output folder
+
+$output_folder = Join-Path -Path $path -ChildPath "RenamedFiles"
+
+if (Test-Path -Path $output_folder) {
+    "Exporting to directory: RenamedFiles"
+} else {
+    New-Item -Path $path -Name "RenamedFiles" -ItemType "directory" -Force
+}
+
+# rename and move files
+
 foreach ($name in $csvnames) {
     foreach ($v in $videos) {
         if($name.BaseName -eq $v.Basename){
-            rename-item $v.fullname -NewName "$($name.NewName)$($v.extension)" -Verbose
+            $new_name = "$($name.NewName)_RI$($v.extension)"
+            $new_name_path = Join-Path -Path $path -ChildPath $new_name
+            rename-item $v.fullname -NewName $new_name -Verbose
+            Move-Item $new_name_path -destination $output_folder
         }
     }
 }
